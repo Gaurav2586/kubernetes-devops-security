aca00e2807425ed37ad77c9b31f82cea4e9dd204
Gaurav2586 <megaurav25@gmail.com>
2022-05-10 15:07:41 +0530

    updated

--- kubesec-scan.sh
+++ kubesec-scan.sh
@@ -1,7 +1,10 @@
 # using kubesec v2 api
-scan_result=$(/usr/bin/curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan)
-scan_message=$(/usr/bin/curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].message -r ) 
-scan_score=$(/usr/bin/curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].score ) 
+#Install Curl
+apt install curl
+
+scan_result=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan)
+scan_message=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].message -r ) 
+scan_score=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].score ) 
 
 
 # using kubesec docker image for scanning
